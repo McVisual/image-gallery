20bf5376a1a76a2f9ecf1b2ad7959d9bd8368afe
mcvisual <jimmy.yoon23@gmail.com>
2019-05-03 21:07:51 -0700

    Added opacity and fade in animation.

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-const current = document.querySelector('#current');
-const imgs = document.querySelectorAll('.imgs img');
+const [current, imgs] = [document.querySelector('#current'), document.querySelectorAll('.imgs img')]
+const opacity = 0.4;
+
+imgs[0].style.opacity = opacity;
 
 imgs.forEach(img => {
-    img.addEventListener('click', e => {
-        current.src = e.target.src;
-    });
+    img.addEventListener('click', imgClick);
 });
+
+function imgClick(e){
+    // Reset the opacity
+    imgs.forEach(img => img.style.opacity = 1);
+
+    // Change current image to clicked image
+    current.src = e.target.src;
+
+    // Add fade in class
+    current.classList.add('fade-in');
+
+    // Remove fade-in class after .5 seconds
+    setTimeout(() => current.classList.remove('fade-in'), 400);
+
+    // Change the opacity to opacity var
+    e.target.style.opacity = opacity;
+}
